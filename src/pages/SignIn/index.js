75ebabf566e67817a8a1f83ed6c47429937e3d79
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-01-06 20:32:01 -0300

    feat: implements yup validation to signin and signup forms

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -14,7 +22,7 @@ export default function SignIn() {
     <>
       <img src={logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
