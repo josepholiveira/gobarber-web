a963a5d3b8f8d3dfc8b3da00d6bee98a21776dc4
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2020-01-07 11:03:13 -0300

    feat: add signout functionality

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 import { Container } from './styles';
 import AvatarInput from './AvatarInput';
@@ -14,6 +15,9 @@ export default function Profile() {
     dispatch(updateProfileRequest(data));
   }
 
+  function handleSignOut() {
+    dispatch(signOut());
+  }
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
@@ -38,7 +42,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do GoBarber</button>
+      <button type="button" onClick={() => handleSignOut()}>
+        Sair do GoBarber
+      </button>
     </Container>
   );
 }
